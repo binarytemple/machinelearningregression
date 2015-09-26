b57188790dbcd4339e7349338f2a8759eab6273b
bryan hunt <github@binarytemple.co.uk>
2015-09-26 12:03:02 +0100

    lunchtime

--- module3.py
+++ module3.py
@@ -8,3 +8,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
 # Code after this
+
+
+bikes_df = pd.read_csv('data/bikes_subsampled.csv')
+temperature = bikes_df['temperature'].values
+bikes_count = bikes_df['count'].values
+
+plt.scatter(temperature, bikes_count, color='k')
+plt.xlabel("temperature")
+plt.ylabel("bikes hired")
+# plt.tight_layout()
+# plt.show()
+
+a = 28
+temperature_predict = np.expand_dims(a=np.linspace(-5,40,100),axis=1)
+bikes_count_predict = a*temperature_predict
+# plt.scatter(temperature, bikes_count, color='k')
+plt.plot(temperature_predict,bikes_count_predict,linewidth=2)
+plt.show()
+
+### next ... a prediction
+
+linear_regression = LinearRegression()
+temperature_ = np.expand_dims(temperature,1)
+linear_regression.fit(temperature_, bikes_count)
+
+print 'Bikes hired at 5 defrees Celsius:', linear_regression.predict(5.)[0]
+# value that minimizes the RMS error
+print 'Optimal slope:' , linear_regression.coef_[0]
+print 'Optimal intercept:', linear_regression.intercept_
--- runme.py
+++ runme.py
@@ -1,7 +1,12 @@
+# noinspection PyUnresolvedReferences
 import module3
+# noinspection PyUnresolvedReferences
 import module4
+# noinspection PyUnresolvedReferences
 import module5
+# noinspection PyUnresolvedReferences
 import module6
+# noinspection PyUnresolvedReferences
 import module7
 
 print "Good to go, all packages installed ok, ready to code."
